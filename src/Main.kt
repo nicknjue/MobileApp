59a0a9a8ee7a3f56a475a860ef8346bedcd6564f
nicknjue <njue.wambui@strathmore.edu>
2026-08-25 14:10:37 +0300

    Lesson 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,28 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+
  println("Hello World!")
+
+  // TENANT iNFO
+
+ println("-----------")
+ println("Welcome to our Tennant Mangement System")
+
+ println("-----------")
+ val tenantName = "Jane Nyakundi"
+ val tenantCar: String = "Mazda"
+ val tenantId = 101
+
+ val unitOccupied = "A-102"
+ val rentDue = 30000
+ val amountPaid = 20000
+
+
+
+  println("Tenant name: $tenantName")
+  println("-----------")
+  println("Amount Paid: $amountPaid")
+
+
 }
